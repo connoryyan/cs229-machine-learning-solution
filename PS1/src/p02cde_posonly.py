7851684a6e4774572aadb52f15331678f5dd0eb9
connoryyan <connor.yan@student.adelaide.edu.au>
2026-04-11 18:30:58 +0930

    Finish PS1 2c.

--- PS1/src/p02cde_posonly.py
+++ PS1/src/p02cde_posonly.py
@@ -3,6 +3,11 @@ import util
 
 from p01b_logreg import LogisticRegression
 
+ds3_train_path = './PS1/data/ds3_train.csv'
+ds3_valid_path = './PS1/data/ds3_valid.csv'
+ds3_test_path = './PS1/data/ds3_test.csv'
+ds3_pred_path = './PS1/output/p02X_pred.txt'
+
 # Character to replace with sub-problem letter in plot_path/pred_path
 WILDCARD = 'X'
 
@@ -21,15 +26,37 @@ def main(train_path, valid_path, test_path, pred_path):
         test_path: Path to CSV file containing test set.
         pred_path: Path to save predictions.
     """
-    pred_path_c = pred_path.replace(WILDCARD, 'c')
-    pred_path_d = pred_path.replace(WILDCARD, 'd')
-    pred_path_e = pred_path.replace(WILDCARD, 'e')
 
     # *** START CODE HERE ***
-    # Part (c): Train and test on true labels
-    # Make sure to save outputs to pred_path_c
+    
     # Part (d): Train on y-labels and test on true labels
     # Make sure to save outputs to pred_path_d
     # Part (e): Apply correction factor using validation set and test on true labels
     # Plot and use np.savetxt to save outputs to pred_path_e
     # *** END CODER HERE
+
+    pred_path_c = pred_path.replace(WILDCARD, 'c')
+    pred_path_d = pred_path.replace(WILDCARD, 'd')
+    pred_path_e = pred_path.replace(WILDCARD, 'e')
+
+    x_train, t_train = util.load_dataset(train_path, label_col='t', add_intercept=True)
+    x_valid, t_valid = util.load_dataset(valid_path, label_col='t',  add_intercept=True)
+    x_test, t_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
+
+    # Part (c): Train and test on true labels
+    # Make sure to save outputs to pred_path_c
+
+    clf = LogisticRegression()
+    clf.fit(x_train, t_train)
+
+    util.plot(x_train, t_train, theta=clf.theta)
+    print("Theta is: ", clf.theta)
+    print("The accuracy on training set is: ", np.mean(clf.predict(x_train) == t_train))
+
+    util.plot(x_test, t_test, clf.theta)
+    t_pred = clf.predict(x_test)
+    print("The accuracy on the test set is: ", np.mean(t_pred == t_test))
+    np.savetxt(pred_path, t_test, '%d')
+
+if __name__ == '__main__':
+    main(ds3_train_path, ds3_valid_path, ds3_test_path, ds3_pred_path)
