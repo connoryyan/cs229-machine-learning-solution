d59b95965e6cba346620c7ed4cc0e7ed10439581
connoryyan <connor.yan@student.adelaide.edu.au>
2026-04-24 00:04:11 +0930

    Finish PS2 p5b

--- PS2/src/p05_percept.py
+++ PS2/src/p05_percept.py
@@ -15,8 +15,7 @@ def initial_state():
 
     """
 
-    # *** START CODE HERE ***
-    # *** END CODE HERE ***
+    return []
 
 
 def predict(state, kernel, x_i):
@@ -32,8 +31,12 @@ def predict(state, kernel, x_i):
     Returns:
         Returns the prediction (i.e 0 or 1)
     """
-    # *** START CODE HERE ***
-    # *** END CODE HERE ***
+
+    s = 0
+    for alpha, x in state:
+        s += alpha * kernel(x, x_i)
+        
+    return sign(s)
 
 
 def update_state(state, kernel, learning_rate, x_i, y_i):
@@ -46,9 +49,9 @@ def update_state(state, kernel, learning_rate, x_i, y_i):
         x_i: A vector containing the features for a single instance
         y_i: A 0 or 1 indicating the label for a single instance
     """
-    # *** START CODE HERE ***
-    # *** END CODE HERE ***
-
+    
+    alpha = learning_rate * (y_i - predict(state, kernel, x_i))
+    state.append((alpha, x_i))
 
 def sign(a):
     """Gets the sign of a scalar input."""
@@ -94,23 +97,23 @@ def train_perceptron(kernel_name, kernel, learning_rate):
         kernel: The kernel function.
         learning_rate: The learning rate for training.
     """
-    train_x, train_y = util.load_csv('../data/ds5_train.csv')
+    train_x, train_y = util.load_csv('./PS2/data/ds5_train.csv')
 
     state = initial_state()
 
     for x_i, y_i in zip(train_x, train_y):
         update_state(state, kernel, learning_rate, x_i, y_i)
 
-    test_x, test_y = util.load_csv('../data/ds5_test.csv')
+    test_x, test_y = util.load_csv('./PS2/data/ds5_test.csv')
 
     plt.figure(figsize=(12, 8))
     util.plot_contour(lambda a: predict(state, kernel, a))
     util.plot_points(test_x, test_y)
-    plt.savefig('./output/p05_{}_output.pdf'.format(kernel_name))
+    plt.savefig('./PS2/output/p05_{}_output.png'.format(kernel_name))
 
     predict_y = [predict(state, kernel, test_x[i, :]) for i in range(test_y.shape[0])]
 
-    np.savetxt('./output/p05_{}_predictions'.format(kernel_name), predict_y)
+    np.savetxt('./PS2/output/p05_{}_predictions'.format(kernel_name), predict_y)
 
 
 def main():
